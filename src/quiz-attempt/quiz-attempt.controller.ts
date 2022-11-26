19d3d45fa64afdb6dc4b8502a4f68aeeba979cba
alfonsus20 <alfonschandrawan@gmail.com>
2022-11-26 15:45:19 +0700

    get quiz result

--- src/quiz-attempt/quiz-attempt.controller.ts
+++ src/quiz-attempt/quiz-attempt.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { GetUser } from '../auth/decorators/get-user.decorator';
+import { JwtGuard } from '../auth/guard';
 import { QuizAttemptService } from './quiz-attempt.service';
 
+@UseGuards(JwtGuard)
 @Controller('quiz-attempt')
 export class QuizAttemptController {
   constructor(private readonly quizAttemptService: QuizAttemptService) {}
+
+  @Get(':id')
+  async getQuizAttemptById(
+    @GetUser('id') userId: number,
+    @Param('id') attemptId: string,
+  ) {
+    return this.quizAttemptService.getQuizAttemptById(userId, attemptId);
+  }
 }
--- src/quiz-attempt/quiz-attempt.service.ts
+++ src/quiz-attempt/quiz-attempt.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common/enums';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { QuizAttempt } from '../models';
@@ -12,10 +13,14 @@ export class QuizAttemptService {
 
   async getQuizAttemptById(userId: number, attemptId: string) {
     try {
-      await this.quizAttemptRepository.findOne({
+      const attempt = await this.quizAttemptRepository.findOne({
         where: { id: +attemptId, user: { id: userId } },
-        relations: { quiz: { questions: { options: true } } },
+        relations: {
+          answers: { question: { options: true }, option: true },
+        },
       });
+
+      return { statusCode: HttpStatus.OK, message: 'success', data: attempt };
     } catch (error) {
       throw error;
     }
