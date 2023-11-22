9cbc5e65327146eb2842f7097f37d379bb4dcc83
alfonsus20 <alfonschandrawan@gmail.com>
2023-11-23 00:48:56 +0700

    trigger pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { statusCode: 200, message: 'Edusex API', data: null };
+    return { statusCode: 200, message: 'Edusex API Update', data: null };
   }
 }
